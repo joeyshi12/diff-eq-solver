0b071aac4a3126749359ff0c6db01362f21b6df4
joeyshi12 <46363213+joeyshi12@users.noreply.github.com>
2019-12-04 19:14:34 -0800

    update

--- src/main/ui/Main.py
+++ src/main/ui/Main.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     # firstOrderODE = FirstOrderODE(lambda x, y: y, 1)
     # firstOrderODE.plot_solution(10, 1000)
 
-    # secondOrderODE = SecondOrderODE(lambda x, y, y_prime: -y, 0, 1)
-    # secondOrderODE.plot_solution(4*np.pi, 100)
-    # x1, x2, y1, y2 = plt.axis()
-    # plt.axis((x1,x2,-2,2))
+    secondOrderODE = SecondOrderODE(lambda x, y, y_prime: -y, 0, 1)
+    secondOrderODE.plot_solution(4*np.pi, 100)
+    x1, x2, y1, y2 = plt.axis()
+    plt.axis((x1,x2,-2,2))
 
     plt.show()
