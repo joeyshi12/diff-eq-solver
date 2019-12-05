Fixed calculation bugs in ODE classes

src/main/model/FirstOrderODE.py
@@ -4,23 +4,21 @@ import matplotlib.pyplot as plt
 
 class FirstOrderODE:
 
-    def __init__(self, function, initial_condition):
+    def __init__(self, function, initial_value):
         self.function = function
-        self. initial_condition = initial_condition
+        self.initial_value = initial_value
 
     def integrate(self, L, n):
         dx = L/n
         y = np.array([])
-        y0 = self.initial_condition
+        y0 = self.initial_value
         for i in range(n):
             y = np.append(y, y0)
-            y1 = y0 + self.function(i*dx, y0)
+            y1 = y0 + self.function(i*dx, y0)*dx
             y0 = y1
         return y
 
     def plot_solution(self, L, n):
         x = np.linspace(0, L, n)
         y = self.integrate(L, n)
-        print(x)
         plt.plot(x, y)
-        plt.show()

src/main/ui/Main.py
@@ -1,9 +1,16 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
 from src.main.model.FirstOrderODE import FirstOrderODE
 from src.main.model.SecondOrderODE import SecondOrderODE
+from src.main.model.HeatEquation import HeatEquation
+import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == '__main__':
-    second_order_ode = SecondOrderODE(lambda x, y, y_prime: -y, 0, 1)
-    second_order_ode.plot_solution(10, 100)
+    # firstOrderODE = FirstOrderODE(lambda x, y: y, 1)
+    # firstOrderODE.plot_solution(10, 1000)
+
+    # secondOrderODE = SecondOrderODE(lambda x, y, y_prime: -y, 0, 1)
+    # secondOrderODE.plot_solution(4*np.pi, 100)
+    # x1, x2, y1, y2 = plt.axis()
+    # plt.axis((x1,x2,-2,2))
+
+    plt.show()