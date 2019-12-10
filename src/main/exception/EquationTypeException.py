05dfe5f3b3d7d3b046e851ab6efd9e6e958998a4
joeyshi12 <46363213+joeyshi12@users.noreply.github.com>
2019-12-09 23:44:17 -0800

    Added EquationTypeException

---
+++ src/main/exception/EquationTypeException.py
@@ -0,0 +1,2 @@
+class EquationTypeException(Exception):
+    pass
--- src/main/model/HeatEquation.py
+++ src/main/model/HeatEquation.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
+from src.main.exception.EquationTypeException import EquationTypeException
+
 
 class HeatEquation:
     alpha: float
@@ -37,6 +39,8 @@ class HeatEquation:
             return self.integrate_mixed_1(dt, dx, k, m, n)
         elif self.boundary_type == 3:
             return self.integrate_mixed_2(dt, dx, k, m, n)
+        else:
+            raise EquationTypeException
 
     def integrate_dirichlet(self, dt: float, dx: float, k: float, m: int, n: int) -> np.array:
         u = []
