d7343cc31aa99537c921b98938eb2f51b3515326
Joey Shi <46363213+joeyshi12@users.noreply.github.com>
2021-01-05 02:34:32 -0800

    updated

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-import os
 import tkinter as tk
 from frontend.mainview import MainView
 
-
-def main():
-    root = tk.Tk()
-    root.resizable(width=False, height=False)
-    root.title("Differential Equation Solver")
-    root.iconbitmap(os.path.join('assets/icon.ico'))
-    root.geometry("1100x600")
-    main_view = MainView(root)
-    main_view.pack(side="top", fill="both", expand=True)
-    root.mainloop()
-
-
-if __name__ == "__main__":
-    main()
+root = tk.Tk()
+root.resizable(width=False, height=False)
+root.title("DE Solver")
+root.iconbitmap('assets/icon.ico')
+root.geometry("1100x600")
+main_view = MainView(root)
+main_view.pack(side="top", fill="both", expand=True)
+root.mainloop()
